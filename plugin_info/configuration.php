7831e8ce6f7941f4adba02b2344670bbb840cedf
tmartinez69009 <tmartinez69009@gmail.com>
2016-05-16 16:54:42 +0200

    Typo

--- plugin_info/configuration.php
+++ plugin_info/configuration.php
@@ -24,7 +24,7 @@ if (!isConnect()) {
 <form class="form-horizontal">
 	<fieldset>
    <div class="form-group">
-    <label class="col-lg-4 control-label">{{Adresse IP du bridge}}</label>
+    <label class="col-lg-4 control-label">{{Adresse IP du Wattcube Web}}</label>
     <div class="col-lg-2">
      <input class="configKey tooltips form-control" data-l1key="bridge_ip" />
    </div>
